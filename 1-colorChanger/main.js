9315c34c8db05af5244a4b2f7bde3f759d031996
professorx3x <git@stackblitz.com>
2024-10-23 01:17:48 +0530

    Initial commit

---
+++ 1-colorChanger/main.js
@@ -0,0 +1,7 @@
+const btn=document.querySelectorAll('.button')
+const body=document.querySelector("body")
+btn.forEach(function(button){
+  console.log(button)
+})
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom_projects
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/professorx3x/dom_projects)
