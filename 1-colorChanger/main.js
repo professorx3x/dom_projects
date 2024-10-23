32bea4a75103481b2ccdcac7ed7188fb594158cb
professorx3x <shubhamsaurav9523@gmail.com>
2024-10-23 21:28:13 +0530

    project1 completed

--- 1-colorChanger/main.js
+++ 1-colorChanger/main.js
@@ -1,7 +1,16 @@
-const btn=document.querySelectorAll('.button')
-const body=document.querySelector("body")
-btn.forEach(function(button){
-  console.log(button)
-})
-
-
+const btn = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+btn.forEach((button) => {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    } else {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
