cee8ff80ab029c42865299e6e34516cacef24009
professorx3x <shubhamsaurav9523@gmail.com>
2024-10-29 21:09:23 +0530

    project 5 done

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,18 @@
-console.log('Project 5');
+const insert = document.getElementById('insert');
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+  <div>
+  <table>
+  <tr>
+  <th>key</th>
+  <th>keyCode</th>
+  <th>code</th>
+  </tr>
+  <tr>
+  <th>${e.key}</th>
+  <th>${e.keyCode}</th>
+  <th>${e.code}</th>
+  </tr>a
+  </div>
+  `;
+});
